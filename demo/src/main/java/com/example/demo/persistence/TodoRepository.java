f39e6d08b43544ab09fd586102ce25be5b83dfbc
roalwh <nmnmnmlk6248@gmail.com>
2023-06-14 23:02:48 +0900

    20230614

--- demo/src/main/java/com/example/demo/persistence/TodoRepository.java
+++ demo/src/main/java/com/example/demo/persistence/TodoRepository.java
@@ -4,7 +4,6 @@ package com.example.demo.persistence;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.example.demo.model.TodoEntity;
@@ -13,9 +12,11 @@ import com.example.demo.model.TodoEntity;
 public interface TodoRepository extends JpaRepository<TodoEntity,String>{
     // select 
     // ?1은 메서드의 매개변수의 순서 위치다. 
-    @Query("select * from Todo t where t.userId=?1")
+    // @Query("select * from Todo t where t.userId=?1")
     //  findByUserId 스프링 데이터 JPA가 메서드 이름을 파싱해서
     //  SELECT * FROM TodoRepository WHERE userId = '{userId}'와 같은 쿼리를 작성해 실행
     //  메서드 이름은 쿼리, 매개변수는 쿼리의 where문에 들어갈 값을 의미
     List<TodoEntity> findByUserId(String userId);
+
+    
 }
--- demo/src/main/java/com/example/demo/service/TodoService.java
+++ demo/src/main/java/com/example/demo/service/TodoService.java
@@ -1,6 +1,8 @@
 package com.example.demo.service;
 
+import java.util.List;
 
+import javax.management.RuntimeErrorException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -8,12 +10,16 @@ import org.springframework.stereotype.Service;
 import com.example.demo.model.TodoEntity;
 import com.example.demo.persistence.TodoRepository;
 
+import lombok.extern.slf4j.Slf4j;
+
+// 로그
+@Slf4j
 @Service
 public class TodoService {
     @Autowired
     private TodoRepository repository;
 
-    public String testService(){
+    public String testService() {
         // TodoEntity 생성
         TodoEntity entity = TodoEntity.builder().title("My first todo item").build();
         System.out.println(entity);
@@ -24,5 +30,28 @@ public class TodoService {
         System.out.println(savedEntity);
         return savedEntity.getTitle();
     }
-}
 
+    // crate 생성 
+    public List<TodoEntity> create(final TodoEntity entity) {
+        // validations
+        // 데이터를 검증
+        validate(entity);
+        // 데이터를 저장
+        repository.save(entity);
+        // 저장후 로그
+        log.info("Entity Id : {} is saved.", entity.getId());
+        
+        return repository.findByUserId(entity.getUserId());
+    }
+    // 검증
+    private void validate(final TodoEntity entity) {
+        if (entity == null) {
+            log.warn("Entity cannot be null.");
+            throw new RuntimeException("Entity cannot be null.");
+        }
+        if (entity.getUserId() == null) {
+            log.warn("Unknown user.");
+            throw new RuntimeException("Unknown user.");
+        }
+    }
+}
