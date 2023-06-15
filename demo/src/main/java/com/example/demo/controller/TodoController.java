f96c4b05312c5093cce6adefbbae3d1b9c916be5
roalwh <nmnmnmlk6248@gmail.com>
2023-06-15 18:21:13 +0900

    20230615

--- demo/src/main/java/com/example/demo/controller/TodoController.java
+++ demo/src/main/java/com/example/demo/controller/TodoController.java
@@ -1,11 +1,9 @@
 package com.example.demo.controller;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.info.ProjectInfoProperties.Build;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,8 +18,13 @@ import com.example.demo.dto.TodoDTO;
 import com.example.demo.model.TodoEntity;
 import com.example.demo.service.TodoService;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @RestController
 @RequestMapping("todo")
+// 로그
+
 public class TodoController {
 
     @Autowired
@@ -84,12 +87,19 @@ public class TodoController {
     // 수정
     @PutMapping
     public ResponseEntity<?> updateTodo(@RequestBody TodoDTO dto) {
+        
         String temporaryUserId = "temporary-user";
+        // dto를 entity로 변환 한다..
         TodoEntity entity = TodoDTO.toEntity(dto);
+        // id를 temporaryUserId 로 초기화
         entity.setUserId(temporaryUserId);
+        // 서비스를 이용해 entity를 업데이트
         List<TodoEntity> entities = service.update(entity);
+        // 자바 스트림을 통해 리턴된 엔ㄷ티티 리스트를 TodoDTO 리스트로 변환
         List<TodoDTO> dtos = entities.stream().map(TodoDTO::new).collect(Collectors.toList());
+        // ResponseDTO 리스트를 이용하여 ResopnseDTO를 초기화
         ResponseDTO<TodoDTO> response = ResponseDTO.<TodoDTO>builder().data(dtos).build();
+        // ResponseDTO 리턴
         return ResponseEntity.ok().body(response);
     }
 
@@ -98,13 +108,20 @@ public class TodoController {
     public ResponseEntity<?> deleteTodo(@RequestBody TodoDTO dto) {
         try {
             String temporaryUserId = "temporary-user";
+            // TodoEntity로 변환
             TodoEntity entity = TodoDTO.toEntity(dto);
+
             entity.setUserId(temporaryUserId);
+            // 서비스를 이용해 entity를 삭제
             List<TodoEntity> entities = service.delete(entity);
+            // 자바 스트림을 이용해 리턴된 엔티티 리스트를 TodoDTO리스트로 변환한다
             List<TodoDTO> dtos = entities.stream().map(TodoDTO::new).collect(Collectors.toList());
+            // 변환된 TOdoDTO 리스트를 이용해 ResponseDTO를 초기화 한다
             ResponseDTO<TodoDTO> response = ResponseDTO.<TodoDTO>builder().data(dtos).build();
+            // ResponseDTO를 리턴
             return ResponseEntity.ok().body(response);
         } catch (Exception e) {
+            //혹시 예외가 있는 경우 dto 대신 error에 메시지를 넣어 리턴한다
             String error = e.getMessage();
             ResponseDTO<TodoDTO> response = ResponseDTO.<TodoDTO>builder().error(error).build();
             return ResponseEntity.badRequest().body(response);
--- demo/src/main/java/com/example/demo/service/TodoService.java
+++ demo/src/main/java/com/example/demo/service/TodoService.java
@@ -55,31 +55,43 @@ public class TodoService {
             throw new RuntimeException("Unknown user.");
         }
     }
+    
     // 검색
     public List<TodoEntity> retrieve(final String userId){
         return repository.findByUserId(userId);
     }
+
     // 업데이트
     public List<TodoEntity> update(final TodoEntity entity){
+        // 저장할 엔티티가 유효한지 확인
         validate(entity);
+        // 검증된 엔티티 id 를 이용하여 TodoEntity를 가져옴
         final Optional<TodoEntity> original = repository.findById(entity.getId());
+        // 반환된 TodoEntity가 존재하면 값을 새 entity 값으로 덮어 씌움
         if(original.isPresent()){
             final TodoEntity todo = original.get();
             todo.setTitle(entity.getTitle());
             todo.setDone(entity.isDone());
+            // 데이터베이스에 새 값을 저장
             repository.save(todo);
         }
+        // Retrieve Todo에서 만든 메서드를 이용하여 모든 Todo 리스트를 호출
         return retrieve(entity.getUserId());
     }
     // 삭제
     public List<TodoEntity> delete(final TodoEntity entity){
+        // 저장할 엔티티가 유효한지 확인한다
         validate(entity);
         try{
+            // 엔티티를 삭제함
             repository.delete(entity);
         } catch(Exception e){
+            // exception 발생시 id와exception을 로깅
             log.error("error deleting entity", entity.getId(),e);
+            //컨트롤러로 exception을 보낸다. 데이터베이스 내부 로직을 캡슐화하려면 e를 리턴하지 않고 새 exception 오브젝트를 리턴
             throw new RuntimeException("error deleting entity"+ entity.getId());
         }
+        // 새 Todo리스트를 가져와 리턴
         return retrieve(entity.getUserId());
     }
 }
