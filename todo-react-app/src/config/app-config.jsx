fec4e5e18934fc74c6a6300df62fb0daa64dafef
roalwh <nmnmnmlk6248@gmail.com>
2023-07-16 18:30:38 +0900

    20230716

--- todo-react-app/src/config/app-config.jsx
+++ todo-react-app/src/config/app-config.jsx
@@ -9,6 +9,6 @@ const hostname = window && window.location && window.location.hostname;
 if(hostname === "localhost"){
     backendHost = "http://localhost:8080";
 } else{
-    backendHost="http://RoalwhtodoListApi.ap-northeast-2.elasticbeanstalk.com";
+    backendHost="http://roalwhtodolistapi.ap-northeast-2.elasticbeanstalk.com";
 }
 export const API_BASE_URL = `${backendHost}`
